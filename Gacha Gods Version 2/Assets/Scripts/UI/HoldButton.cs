2710c402fea550745d9868d585071c4c2d8a6fcf
Joshua Tan <joshuatan0806@gmail.com>
2022-08-22 20:33:59 +0930

    hold button

---
+++ Gacha Gods Version 2/Assets/Scripts/UI/HoldButton.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HoldButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] float timeToHold;
+    bool isBeingHovered = false;
+    public System.Action ActionToExecute;
+    bool isBeingHeld;
+    float timer = 0;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isBeingHovered = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isBeingHeld = false;
+        isBeingHovered = false;
+        timer = timeToHold;
+    }
+
+    private void Awake()
+    {
+        timer = timeToHold;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isBeingHovered)
+            isBeingHeld = true;
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isBeingHeld = false;
+            timer = timeToHold;
+        }
+
+        if(isBeingHeld)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer < 0)
+                Execute();
+        }
+    }
+
+    void Execute()
+    {
+        ActionToExecute?.Invoke();
+        isBeingHovered = false;
+        timer = timeToHold;
+    }
+}
--- Gacha Gods Version 2/Assets/Scripts/UI/ViewCharacterPreview.cs
+++ Gacha Gods Version 2/Assets/Scripts/UI/ViewCharacterPreview.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(HoldButton))]
 public class ViewCharacterPreview : MonoBehaviour
 {
     Character character;
     CharacterPreview preview;
-    Button button;
+    HoldButton button;
 
     private void Awake()
     {
-        button = GetComponent<Button>();
+        button = GetComponent<HoldButton>();
+    }
+
+    private void OnEnable()
+    {
+        button.ActionToExecute += PreviewCharacter;
+    }
+
+    private void OnDisable()
+    {
+        button.ActionToExecute -= PreviewCharacter;
     }
 
     public void Initialise(Character character)
     {
-        button.onClick.RemoveAllListeners();
-        button.onClick.AddListener(() => PreviewCharacter());
-
         this.character = character;
         GetComponent<Image>().sprite = character.Icon;
     }
-
+ 
     void PreviewCharacter()
     {
         if (character == null)
