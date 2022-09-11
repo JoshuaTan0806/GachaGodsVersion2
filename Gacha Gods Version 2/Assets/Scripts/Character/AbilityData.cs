d33579d3c84d59f0ae84049a9a538d2cda45f607
Joshua Tan <joshuatan0806@gmail.com>
2022-09-11 20:48:57 +0930

    ability data

--- Gacha Gods Version 2/Assets/Scripts/Character/AbilityData.cs
+++ Gacha Gods Version 2/Assets/Scripts/Character/AbilityData.cs
@@ -10,4 +10,31 @@ public class AbilityData : ScriptableObject
 
     public string Description => description;
     [SerializeField] string description;
+
+    public int MaxRange => maxRange;
+    [SerializeField] int maxRange = 1;
+
+    public int NumberOfTargets => numberOfTargets;
+    [SerializeField] int numberOfTargets = 1;
+
+    public int NumberOfSpawns => numberOfSpawns;
+    [SerializeField] int numberOfSpawns = 1;
+
+    public TargetType TargetType => targetType;
+    [SerializeField] TargetType targetType;
+}
+
+public enum TargetType
+{
+    CurrentEnemy,
+    ClosestEnemy,
+    FurthestEnemy,
+    HighestHealthEnemy,
+    LowestHealthEnemy,
+}
+
+public enum AbilityType
+{
+    SpawnDirectlyOnTarget,
+    Projectile
 }
