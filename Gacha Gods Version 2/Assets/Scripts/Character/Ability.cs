8a5f50e22c431dd128f544e93db4d7fd909d9055
Joshua Tan <joshuatan0806@gmail.com>
2022-09-11 22:31:14 +0930

    ability

--- Gacha Gods Version 2/Assets/Scripts/Character/Ability.cs
+++ Gacha Gods Version 2/Assets/Scripts/Character/Ability.cs
@@ -6,8 +6,25 @@ public class Ability : MonoBehaviour
 {
     CharacterStats stats;
 
+    private void OnEnable()
+    {
+        GameManager.OnBattleWon += DestroyGameobject;
+        GameManager.OnBattleLost += DestroyGameobject;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnBattleWon -= DestroyGameobject;
+        GameManager.OnBattleLost -= DestroyGameobject;
+    }
+
     public void Initialise(CharacterStats stats)
     {
         this.stats = stats;
+    }
+
+    void DestroyGameobject()
+    {
+        Destroy(gameObject);
     }
 }
